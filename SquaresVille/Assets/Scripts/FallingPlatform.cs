704954e089b8527ba02ffebbf9e440237d439e7c
jono mannion <johnmannion51@gmail.com>
2018-11-12 22:24:02 +0000

    added music to levels

--- SquaresVille/Assets/Scripts/FallingPlatform.cs
+++ SquaresVille/Assets/Scripts/FallingPlatform.cs
@@ -5,89 +5,89 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour {
 
     Rigidbody2D rb2d;
-    //public float FallDelay;
+    public float FallDelay;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
 
     }
-    void onCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.name.Equals("red"))
-        {
-            Invoke("dropPlatform", 0.2f);
-            Destroy(gameObject, 2f);
-        }
-        Debug.Log(col.collider.tag);
-    }
+    /* void onCollisionEnter2D(Collision2D col)
+     {
+         if (col.gameObject.name.Equals("red"))
+         {
+             Invoke("dropPlatform", 0.2f);
+             Destroy(gameObject, 2f);
+         }
+         Debug.Log(col.collider.tag);
+     }
 
-    void dropPlatform()
-    {
+     void dropPlatform()
+     {
 
-        rb2d.isKinematic = false;
+         rb2d.isKinematic = false;
 
-    }
-    /*void OnCollisonEnter2D(Collision2D col)
-    {
-        if (col.collider.CompareTag("red"))
-        {
-            StartCoroutine("Fall");
-        }
-
-    }
-    IEnumerator Fall()
-    {
-        yield return new WaitForSeconds(FallDelay);
-        rb2d.isKinematic = false;
-        yield return 0;
-    }
-    public Collider coll;
-
-    void Start()
-    {
-        coll = GetComponent<Collider>();
-        coll.isTrigger = true;
-    }
-
-    // Disables gravity on all rigidbodies entering this collider.
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.attachedRigidbody)
-            other.attachedRigidbody.useGravity = false;
-    }
-
-
-
+     }*/
     void OnCollisonEnter2D(Collision2D col)
-    {
-        if (col.collider.CompareTag("red"))
-        {
-            StartCoroutine("Fall");
-        }
-        
-    }
-    IEnumerator Fall()
-    {
-        yield return new WaitForSeconds(FallDelay);
-        rb2d.isKinematic = false;
-        yield return 0;
-    }
+     {
+         if (col.collider.CompareTag("red"))
+         {
+             StartCoroutine("Fall");
+         }
 
-    void onCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.name.Equals("red"))
-        {
-            Invoke("dropPlatform", 0.5f );
-            Destroy(gameObject, 2f);
-        }
-        Debug.Log(col.collider.tag);
-    }
+     }
+     IEnumerator Fall()
+     {
+         yield return new WaitForSeconds(FallDelay);
+         rb2d.isKinematic = false;
+         yield return 0;
+     }
+    /*public Collider coll;
 
-   void dropPlatform()
-    {
-        
-        rb2d.isKinematic = false;
+         void Start()
+         {
+             coll = GetComponent<Collider>();
+             coll.isTrigger = true;
+         }
 
-    }*/
+         // Disables gravity on all rigidbodies entering this collider.
+         void OnTriggerEnter(Collider other)
+         {
+             if (other.attachedRigidbody)
+                 other.attachedRigidbody.useGravity = false;
+         }
+
+
+
+         void OnCollisonEnter2D(Collision2D col)
+         {
+             if (col.collider.CompareTag("red"))
+             {
+                 StartCoroutine("Fall");
+             }
+
+         }
+         IEnumerator Fall()
+         {
+             yield return new WaitForSeconds(FallDelay);
+             rb2d.isKinematic = false;
+             yield return 0;
+         }
+
+         void onCollisionEnter2D(Collision2D col)
+         {
+             if (col.gameObject.name.Equals("red"))
+             {
+                 Invoke("dropPlatform", 0.5f );
+                 Destroy(gameObject, 2f);
+             }
+             Debug.Log(col.collider.tag);
+         }
+
+        void dropPlatform()
+         {
+
+             rb2d.isKinematic = false;
+
+         }*/
 }
---
+++ SquaresVille/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public string startLevel;
+    public string levelSelect;
+
+    public void NewGame()
+    {
+        Application.LoadLevel("Tutorial");
+    }
+
+    public void LevelSelect()
+    {
+
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
